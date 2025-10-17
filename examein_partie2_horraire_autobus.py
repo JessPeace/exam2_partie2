38d7aead2da245c9748d8834a5272eec4d23cf12
1422509 <1422509@cegepoutaouais.qc.ca>
2025-10-17 12:22:46 -0400

    push #3

--- examein_partie2_horraire_autobus.py
+++ examein_partie2_horraire_autobus.py
@@ -1,10 +1,21 @@
-import datetime
-
-#aujourdhui=datetime.now()
-#print(aujourdhui.strftime('%y/%m/%d , %H:%M'))
+from datetime import *
 
 print('lhorraire de passage du buss hanté est:\n6:14\n7:44\n9:14\n10:44\n12:14\n13:44\n15:14\n16:44\n18:14\n19:44\n21:14.')
 
+aujourdhui=datetime.today()
+print(aujourdhui.strftime('il est %H:%M '))
 
 
-#if __name__ == "__main__":
+def heure_de_passage(intervales, minutes):
+    '''
+    foonction qui vas calculer les minutes restants le prochain passage du bus hanté:
+    :param intervales: 90 minutes
+    :param minutes: minutes avant le procchain passage
+    :return: minutes restants avant le prochain passage du bus hanté.
+    '''
+    intervales = abs(aujourdhui.date() - heure_de_passage )
+    print(intervales.minutes-90,'minutes avant le passage du buss hanté.')
+
+
+if __name__ == "__main__":
+
