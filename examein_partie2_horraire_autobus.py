2ac9feffc50fae984b935acebcd42f3395da99e9
1422509 <1422509@cegepoutaouais.qc.ca>
2025-10-17 12:11:44 -0400

    push #2

---
+++ examein_partie2_horraire_autobus.py
@@ -0,0 +1,10 @@
+import datetime
+
+#aujourdhui=datetime.now()
+#print(aujourdhui.strftime('%y/%m/%d , %H:%M'))
+
+print('lhorraire de passage du buss hanté est:\n6:14\n7:44\n9:14\n10:44\n12:14\n13:44\n15:14\n16:44\n18:14\n19:44\n21:14.')
+
+
+
+#if __name__ == "__main__":
