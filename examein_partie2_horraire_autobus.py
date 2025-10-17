4a6d630b26ca5ccb522190994f035eaa3154b234
1422509 <1422509@cegepoutaouais.qc.ca>
2025-10-17 13:48:59 -0400

    push #11.........

--- examein_partie2_horraire_autobus.py
+++ examein_partie2_horraire_autobus.py
@@ -2,12 +2,15 @@ from datetime import *
 
 #je sais pas pourquoi le code focntionne pas. je vais dans mes notes et il y a rien qui débloque.
 
-horraire=[ '6:14', '7:44', '9:14', '10:44', '12:14', '13:44', '15:14', '16:44', '18:14' , '19:44', '21:14' ] #ça c'est la liste pour python.
+horraire=[ '6:14', '7:44', '9:14', '10:44', '12:14', '13:44', '15:14', '16:44', '18:14' , '19:44', '21:14' ]
+#ça c'est la liste pour python.^^^^
 
 heure= '12:12'
-#heure = (input('quel heure est til?')) #je veux vraiment que python figure que j'essaie de lui donner une valeur avec l'utilisateur mais il comprends pas c'est quoi. Et sa me rends folle.
+#heure = (input('quel heure est til?'))
+#je veux vraiment que python figure que j'essaie de lui donner une valeur avec l'utilisateur mais il comprends pas c'est quoi.
+# Et sa me rends folle.
 minutes = 90
-
+#je regades toutes mes lignes et il y a rien qui cloche.....
 def heure_de_passage(heure, minutes, intervales):
     '''
     fonction qui vas calculer les minutes restants le prochain passage du bus hanté:
@@ -18,20 +21,22 @@ def heure_de_passage(heure, minutes, intervales):
     try:
         intervales = (heure-minutes)
     except IndentationError and TypeError:
-        if  intervales<= 15:             #là j'essaie de faire en sorte que python fasse une soustraction des minutes sur l'heure pour renvoiller le temps restant.
+        if  intervales<= 15:
+#là j'essaie de faire en sorte que python fasse une soustraction des minutes sur l'heure pour renvoiller le temps restant.
             print(f" l'autobuss passe à [{horraire}]")
             print(f"le buss arriveras dans [{intervales}] minutes.")
     if intervales <=30:
         print(f" l'autobuss passe à [{horraire}]")
         print(f"le buss arriveras dans [{intervales}] minutes.")
-        print(intervales)
+        return intervales
 
 #il y a rien qui fontionne. je fais mon possible mais rien fonctionne sauf mes print.
 
 if __name__ == "__main__":
     minutes = 90
-    print("l'horraire de passage du buss hanté est:\n6:14\n7:44\n9:14\n10:44\n12:14\n13:44\n15:14\n16:44\n18:14\n19:44\n21:14.") #ça c'est pour montrer à l'utilisateur l'horraire du bus, c'est pas une liste pour python.
-    heure_de_passage(heure-minutes)
+    print("l'horraire de passage du buss hanté est:\n6:14\n7:44\n9:14\n10:44\n12:14\n13:44\n15:14\n16:44\n18:14\n19:44\n21:14.")
+    #ça c'est pour montrer à l'utilisateur l'horraire du bus, c'est pas une liste pour python. ^^^^
+    heure_de_passage(heure,minutes,intervales)
 
 #'TypeError: unsupported operand type(s) for -: 'str' and 'int'
 # je comprends pas comment réperer cette erreur là.
