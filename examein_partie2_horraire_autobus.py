57ea584dcdda28705c171be6602b19ccaa4e0a0c
1422509 <1422509@cegepoutaouais.qc.ca>
2025-10-17 13:07:17 -0400

    push #6

--- examein_partie2_horraire_autobus.py
+++ examein_partie2_horraire_autobus.py
@@ -1,25 +1,29 @@
 from datetime import *
 
-print('lhorraire de passage du buss hanté est:\n6:14\n7:44\n9:14\n10:44\n12:14\n13:44\n15:14\n16:44\n18:14\n19:44\n21:14.')
+#print("l'horraire de passage du buss hanté est:\n6:14\n7:44\n9:14\n10:44\n12:14\n13:44\n15:14\n16:44\n18:14\n19:44\n21:14.")
+
+horraire=[ '6:14', '7:44', '9:14', '10:44', '12:14', '13:44', '15:14', '16:44', '18:14' , '19:44', '21:14' ]
 
 '''aujourdhui = datetime.now()
 print(aujourdhui.strftime('il est %H:%M '))'''
 
-#heure=input('quel heure est til?')
-heure=('12:10') ##test##
 
-def heure_de_passage(minutes):
+
+def heure_de_passage(heure, intervales, minutes):
     '''
     fonction qui vas calculer les minutes restants le prochain passage du bus hanté:
     :param intervales: 90 minutes
     :param minutes: minutes avant le procchain passage
     :return: minutes restants avant le prochain passage du bus hanté.
     '''
-    intervales = ( heure % minutes )
+    intervales = int(heure - minutes)
     if intervales <= 15:
-        print('le buss arriveras dans 15 minutes.')
+        print(f" l'autobuss passe à [{horraire}]")
+        print(f"le buss arriveras dans [{intervales}] minutes.")
     elif intervales <=30:
-        print('le buss arriveras dans 30 minutes.')
+        print(f" l'autobuss passe à [{horraire}]")
+        print(f"le buss arriveras dans [{intervales}] minutes.")
+        print(intervales)
         return intervales
 
 
@@ -27,7 +31,8 @@ def heure_de_passage(minutes):
 #abs(aujourdhui.date() - la_date):
 
 if __name__ == "__main__":
-
-    heure_de_passage(minute=90)
+    #heure=input('quel heure est til?')
+    heure=('12:10')  ##test##
+    heure_de_passage(intervales=90, heure=heure, minutes=0)
 
     #là j'essaie de faire en sorte que python fasse une soustraction des minutes sur l'heure
